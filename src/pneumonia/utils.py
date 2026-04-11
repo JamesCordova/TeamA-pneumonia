4bb120ec96d8c29d6afe23581ffb188cf86d26e2
JamesCordova <jcordovas@gmail.com>
2026-04-10 20:55:08 -0500

    feat: add data loading and processing module with database connection functionality

---
+++ src/pneumonia/utils.py
@@ -0,0 +1,28 @@
+"""
+Utility functions
+"""
+
+import logging
+
+def setup_logger(name: str, level: str = "INFO") -> logging.Logger:
+    """
+    Setup logger for a module
+    
+    Args:
+        name: Logger name
+        level: Logging level
+        
+    Returns:
+        Configured logger instance
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    
+    return logger
